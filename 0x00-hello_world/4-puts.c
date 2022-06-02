d0e609f296da0b8bfcd9c0e8e4b3ee2c42872d95
temikiki <temitayokiki@gmail.com>
2022-06-02 14:36:03 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Return: Always 0 (success)
@@ -9,7 +8,7 @@
 
 int main(void)
 {
-	puts("programing is like building a multilingual puzzle /n");
+	puts("programing is like building a multilingual puzzle");
 
 	return (0);
 }
