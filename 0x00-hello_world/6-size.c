2d127cd31caf05fce4e75db2a4ec6535af33159a
temikiki <temitayokiki@gmail.com>
2022-06-03 03:09:32 -0700

    t

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("%lu\n", sizeof(char));
+	printf("%lu\n", sizeof(int));
+	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(double));
+	return (0);
+
+}
