6a3c1bdd4a4b4acd11dfad26fbd3ea588898487f
temikiki <temitayokiki@gmail.com>
2022-06-03 17:37:01 -0700

    t

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,13 @@
 
 int main(void)
 {
-        char i = 'a';
+ 	char i = 'a';
 	char t = 'A';
-
-        for (i = 'a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
         putchar(i);
 	for (t = 'A'; t <= 'Z'; t++)
 	putchar(t);
         putchar('\n');
-        return (0);
+	return (0);
 }
 
