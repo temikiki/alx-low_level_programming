f3862d6e552bb88eef4b37ecbccdc77101537186
temikiki <temitayokiki@gmail.com>
2022-06-04 05:33:32 -0700

    add

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char hi;
+	int x;
+	for (x = 47; x <= 57; x++)
+	
+	{
+	putchar(x);
+	}
+	for (hi = 'a'; hi <= 'f'; hi++)
+	
+	{
+	putchar(hi);
+	}
+	return (0);
+}
