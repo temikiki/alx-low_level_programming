e935679c75783e4ae7b1af28cb36ddce6edc0599
temikiki <temitayokiki@gmail.com>
2022-06-06 01:00:33 -0700

    100

---
+++ 0x01-variables_if_else_while/100-print_combs.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int e;
+
+	i = 0;
+	e = 0;
+
+	while (e < 10)
+	{ 
+		i = 0;
+		while (i < 10)
+		{ if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if ( i == 9 && e == 8)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			i++;
+		}
+		e++;
+	}
+	putchar('\n');
+	return(0);
+}
