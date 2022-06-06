91987ea90ae477b515452354fac1cef3a864a68f
temikiki <temitayokiki@gmail.com>
2022-06-06 01:53:33 -0700

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int e;
+	int a;
+	i = 48;
+	e = 48;
+	a = 48;
+	while (e < 58)
+	{
+		i = 48;
+		 while (i < 58)
+		 {
+			 a = 48;
+			 while (a < 58)
+			 {
+				 if ( e != i && e != a && i != a && e < i && i <a)
+				 {
+					 putchar(e);
+					 putchar(i);
+					 putchar(a);
+					 if (i == 56 && e == 55 && a == 57)
+					 {
+					 	break;
+					 }
+					 putchar(',');
+					 putchar(' ');
+				}
+				 a++;
+			 }
+			 i++;
+		 }
+		 e++;
+	}
+	putchar('\n');
+	return (0);
+}
+
