b41faae657af5ffeb372a7d74d1f2a7e38e088bf
temikiki <temitayokiki@gmail.com>
2022-06-07 05:16:07 -0700

    f

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -9,8 +10,24 @@
 
 int main(void)
 {
-	int i;
-       	i = 4;
-	putchar(i);
+	char i[9];
+
+       	i[0] = '_';
+	i[1] = 'p';
+	i[2] = 'u';
+	i[3] = 't';
+	i[4] = 'c';
+	i[5] = 'h';
+	i[6] = 'a';
+	i[7] = 'r';
+	i[8] = '\0';
+
+	int a;
+	for (a =0; a < 9; a++)
+	{
+	putchar(a);
+	}
+
+	putchar('\n');
 	return (0);
 }
