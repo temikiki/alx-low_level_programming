a865242a634015fa61e9def01a94deedd77e1660
temikiki <temitayokiki@gmail.com>
2022-06-07 07:00:21 -0700

    d

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower(int c) - returns 1 for lower case and 0 for uppercase
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	
+	if ((c = 95) && (c <= 122))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
