ae9a4092c6ccfe43ccbfce031ca7ebc15394a63c
temikiki <temitayokiki@gmail.com>
2022-06-08 06:28:20 -0700

    d

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,14 @@
 /**
  * _isalpha - Entry point
  *
+ * @c: the cahracter in ASCII code
+ *
  * Return: 1 or 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 || c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 || c <= 122))
 	{
 	return (1);
 	}
