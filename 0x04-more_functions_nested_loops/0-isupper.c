1f6d01b36be9f1de11a2330bc38e1f592fabfa83
temikiki <temitayokiki@gmail.com>
2022-06-09 06:43:35 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 1 or 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 65; c <= 90; c++)
+	{
+	return (1);
+	}
+	return (0);
+}
