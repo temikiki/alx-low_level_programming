c6e6471a8b14c555a8061e7433274a9bcd244cb5
temikiki <temitayokiki@gmail.com>
2022-06-10 02:25:33 -0700

    d

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * int mul - multiples two numbers
+ *@a: an input integer 
+ *@b: an input integer
+ * Return: ALways 0
+ */
+
+int mul(int a, int b)
+{
+	putchar(a * b);
+}	
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@
 void _putchar();
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
