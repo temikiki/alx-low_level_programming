de96190d08acfe8ade735972e3f41302aad36dbd
temikiki <temitayokiki@gmail.com>
2022-06-13 19:23:44 -0700

    commit 8

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - takes string as input
+ *
+ * @str: string to be taken
+ */
+
+void puts_half(char *str)
+{
+	int a, b = 0;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		b += 1;
+	}
+	for (a = (_strlen(str) + b) / 2; a < _strlen(str); a++)
+	{
+		putchar(str[a]);
+	}
+	putchar('\n');
+}
