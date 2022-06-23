386048b257d938366af6cb92f49840e5cd1d7a8d
temikiki <temitayokiki@gmail.com>
2022-06-22 18:19:43 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the lenght of the string
+ * @s: an input string to be counted
+ *
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
